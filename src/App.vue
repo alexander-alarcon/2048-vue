a23447b0993a68e11521d741fa3e24f23d350890
Alexander Alarcon <walarcon14@gmail.com>
2022-06-15 14:18:26 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Hello World!')
+
+message.value = 'Hello Vue!'
+
+</script>
+
+<template>
+  <h1>2048 clone</h1>
+  <p>{{ message }}</p>
+</template>
