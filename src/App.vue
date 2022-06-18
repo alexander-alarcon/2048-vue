518007e876559d7f63fccd423f0e9573636b2d38
Alexander Alarcon <walarcon14@gmail.com>
2022-06-18 09:40:13 -0500

    Add contols and listeners to the game

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <script setup>
-import GameBoard from './components/GameBoard.vue'
-
+import GameBoard from "./components/GameBoard.vue";
 </script>
 
 <template>
-  <h1 class="text-3xl text-stone-800 p-4">2048 clone</h1>
+  <h1 class="p-4 text-3xl text-stone-800">2048 clone</h1>
   <game-board />
 </template>
--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -6,10 +6,39 @@ import useMainStore from "../stores/main";
 
 const mainStore = useMainStore();
 const { board, cellCount, boardSize } = storeToRefs(mainStore);
+
+window.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowUp") {
+    mainStore.moveUp();
+  } else if (event.key === "ArrowDown") {
+    mainStore.moveDown();
+  } else if (event.key === "ArrowLeft") {
+    mainStore.moveLeft();
+  } else if (event.key === "ArrowRight") {
+    mainStore.moveRight();
+  }
+});
 </script>
 
 <template>
   <div class="container mx-auto">
+    <div class="flex w-full justify-end">
+      <button
+        v-if="canGoBack"
+        class="m-4 rounded bg-[#8f7a66]/75 px-6 py-2 font-bold text-white"
+        type="button"
+        @click="mainStore.goBack()"
+      >
+        Back
+      </button>
+      <button
+        class="m-4 rounded bg-[#8f7a66] px-6 py-2 font-bold text-white"
+        @click="mainStore.$reset"
+      >
+        New game
+      </button>
+    </div>
+
     <div
       class="game-board mx-auto grid aspect-square min-w-[320px] max-w-md gap-2 rounded-lg bg-board p-2"
       :style="{ gridTemplateColumns: `repeat(${board.length}, 1fr)` }"
@@ -22,10 +51,7 @@ const { board, cellCount, boardSize } = storeToRefs(mainStore);
             Math.floor((cell - 1) % boardSize)
           ]
         "
-        :index="cell"
       />
-
-      <!--     <game-cell v-for="(cell, index) in cellCount" :key="index" :cell="cell" /> -->
     </div>
   </div>
 </template>
